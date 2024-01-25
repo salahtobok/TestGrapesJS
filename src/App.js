1af947caa648d2724b9b9ab9a068d97f057fd402
salaheddine.tobok <a200mC5#?C>
2024-01-25 11:44:58 +0100

    Add new blocks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import grapesJS from "grapesjs";
+import gjsBlocksBasic from 'grapesjs-blocks-basic';
+import grapesjsPluginForms from 'grapesjs-plugin-forms';
+import grapesjsComponentCountdown from 'grapesjs-component-countdown';
+import grapesjsPluginExport from 'grapesjs-plugin-export';
+import grapesjsTabs from 'grapesjs-tabs';
+import grapesjsCustomCode from 'grapesjs-custom-code';
+import grapesjsTouch from 'grapesjs-touch';
+import grapesjsParserPostcss from 'grapesjs-parser-postcss';
+import grapesjsTooltip from 'grapesjs-tooltip';
+import grapesjsTuiImageEditor from 'grapesjs-tui-image-editor';
+import grapesjsTyped from 'grapesjs-typed';
+import grapesjsStyleBg from 'grapesjs-style-bg';
+import grapesjsPresetWebpage from 'grapesjs-preset-webpage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [editor, setEditor] = useState(null)
+
+    useEffect(() => {
+        const editor = grapesJS.init({
+            container: "#editor",
+            plugins: [
+                gjsBlocksBasic,
+                grapesjsPluginForms,
+                grapesjsComponentCountdown,
+                grapesjsPluginExport,
+                grapesjsTabs,
+                grapesjsCustomCode,
+                grapesjsTouch,
+                grapesjsParserPostcss,
+                grapesjsTooltip,
+                grapesjsTuiImageEditor,
+                grapesjsTyped,
+                grapesjsStyleBg,
+                grapesjsPresetWebpage,
+            ],
+            pluginsOpts: {
+                gjsBlocksBasic : {},
+                grapesjsPluginForms : {},
+                grapesjsComponentCountdown : {},
+                grapesjsPluginExport : {},
+                grapesjsTabs : {},
+                grapesjsCustomCode : {},
+                grapesjsTouch : {},
+                grapesjsParserPostcss : {},
+                grapesjsTooltip : {},
+                grapesjsTuiImageEditor : {},
+                grapesjsTyped : {},
+                grapesjsStyleBg : {},
+                grapesjsPresetWebpage : {},
+            },
+        });
+        setEditor(editor);
+    }, [])
+    return (
+        <div className="App">
+            <div id="editor"></div>
+        </div>
+    );
 }
 
 export default App;
